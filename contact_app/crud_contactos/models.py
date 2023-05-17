ad90fd9c86a8e8a9b0447ac2ce76c084a503c4a7
Lizandro Lozano <llozano@outlook.com>
2023-05-17 13:37:18 -0500

    created the CrudContacts model

--- contact_app/crud_contactos/models.py
+++ contact_app/crud_contactos/models.py
@@ -1,3 +1,17 @@
+# third parties
 from django.db import models
+from django.db.models.fields import IntegerField, CharField, EmailField
 
-# Create your models here.
+
+class CrudContacts(models.Model):    
+    first_name = CharField(max_length=50)
+    last_name = CharField(max_length=50)
+    company = CharField(max_length=100)
+    job_title = CharField(max_length=50)
+    phone = CharField(max_length=12)
+    email = EmailField(max_length=100)
+    cel_phone = CharField(max_length=12)
+    note = CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return f'<Contact: "{self.first_name}, {self.last_name}">'
