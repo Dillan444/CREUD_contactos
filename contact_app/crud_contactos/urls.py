b94e7bc7141fd956c2b718d6dd309b406a49c430
Dillan444 <rojasdylan444@gmail.com>
2023-05-20 11:25:51 -0500

    Added CRUD views

--- contact_app/crud_contactos/urls.py
+++ contact_app/crud_contactos/urls.py
@@ -7,4 +7,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new_contact", views.new_contact, name="new_contact"),
+    path("contact_update/<int:contact_id>", views.contact_update, name="contact_update"),
+    path("contact_delete/<int:contact_id>", views.contact_delete, name="contact_delete"),
+    path("contact_detail/<int:contact_id>", views.contact_detail, name="contact_detail")
 ]
--- contact_app/crud_contactos/views.py
+++ contact_app/crud_contactos/views.py
@@ -7,3 +7,19 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'home.html')
+
+
+def new_contact(request):
+    return render(request, "new_contact.html")
+
+
+def contact_update(request, contact_id):
+    return render(request, "contact_update.html", {"contact_id": contact_id})
+
+
+def contact_delete(request, contact_id):
+    return render(request, "contact_delate.html", {"contact_id": contact_id})
+
+
+def contact_detail(request, contact_id):
+    return render(request, "contact_detail.html", {"contact_id": contact_id})
