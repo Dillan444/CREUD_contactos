73899e1d65c46a9dd38c5948ab5941855acc091e
Lizandro Lozano <llozano@outlook.com>
2023-05-26 12:40:42 -0500

    implementing the new contact and index routes

--- contact_app/crud_contactos/views.py
+++ contact_app/crud_contactos/views.py
@@ -1,16 +1,50 @@
 # third parties
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # local
-
+from crud_contactos.models import CrudContacts
+from crud_contactos.forms import CreateContact
 
 def index(request):
-    return render(request, 'home.html')
+    query_contacts = CrudContacts.objects.all()
+
+    context = {
+        'show_contacts': query_contacts
+    }
+    return render(request, 'home.html', context)
 
 
 def new_contact(request):
-    return render(request, "new_contact.html")
+    create_contact = CreateContact()
+
+    context = {
+        'create_contact': create_contact
+    }
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        company = request.POST['company']
+        job_title = request.POST['job_title']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        cel_phone = request.POST['cel_phone']
+        note = request.POST['note']
+
+        new_contact = CrudContacts(
+            first_name = first_name,
+            last_name = last_name,
+            company = company,
+            job_title = job_title,
+            phone = phone,
+            email = email,
+            cel_phone = cel_phone,
+            note = note,
+        )
+        new_contact.save()
+        redirect('/')
+            
+    return render(request, "new_contact.html", context)
 
 
 def contact_update(request, contact_id):
